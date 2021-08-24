d1ac4191e7e376407e75834e28aeba6f52afaaf7
Vipra7 <46512898+Vipra7@users.noreply.github.com>
2021-08-24 23:36:32 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,18 @@
+import pandas as pd
 import streamlit as st
+import mysql.connector
+
+connection = mysql.connector.connect(**st.secrets["mysql"])
+
+if connection.is_connected()==False:
+    print("Not Connected")
+if connection.is_connected()==True:
+    print("Connected")
+
+def create_user_df():
+    df = pd.read_sql_query("select  * from mytable", connection)
+    return df
+df=create_user_df()
+
 st.write("Hello World")
+st.write(df)
